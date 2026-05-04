9a6a59ae7e840a1f52538d294ae149d7fe6a1ea2
NaikoLeFou <joan.courty@ynov.com>
2026-05-04 10:57:37 +0200

    feat(Capsule, Orion,CargoDragon) Creation of the abstract Capsule class and its Orion and Cargo Dragon models

---
+++ Capsule.java
@@ -0,0 +1,35 @@
+public abstract class Capsule {
+    protected String nom;
+    protected boolean estHabitee;
+    protected int occupantsMax;
+    protected double masse;
+    protected double prix;
+
+    public Capsule(String nom, boolean estHabitee, int occupantsMax, double masse, double prix) {
+        this.nom = nom;
+        this.estHabitee = estHabitee;
+        this.occupantsMax = occupantsMax;
+        this.masse = masse;
+        this.prix = prix;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public boolean isHabitee() {
+        return estHabitee;
+    }
+
+    public int getOccupantsMax() {
+        return occupantsMax;
+    }
+
+    public double getMasse() {
+        return masse;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+}
