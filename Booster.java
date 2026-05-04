e4696d17d752c625002ebbcadd43669262c246bb
NaikoLeFou <joan.courty@ynov.com>
2026-05-04 10:27:59 +0200

    feat(Booster) : Added a system of booster

---
+++ Booster.java
@@ -0,0 +1,29 @@
+public class Booster {
+    private String nom;
+    private int pousseeAdditionnelle; 
+    private double masse; 
+    private double prix; 
+
+    public Booster(String nom, int pousseeAdditionnelle, double masse, double prix) {
+        this.nom = nom;
+        this.pousseeAdditionnelle = pousseeAdditionnelle;
+        this.masse = masse;
+        this.prix = prix;
+    }
+
+    public String getNom() { 
+        return nom; 
+    }
+
+    public int getPousseeAdditionnelle() { 
+        return pousseeAdditionnelle; 
+    }
+
+    public double getMasse() { 
+        return masse; 
+    }
+
+    public double getPrix() { 
+        return prix; 
+    }
+}
